c9fcd087a17e30f2a1d2f404ccbcedbd4d698dd9
Railson Martins <railsonmartins1970@gmail.com>
2024-05-13 21:41:45 -0300

    Create basic class

--- js/character_registration.js
+++ js/character_registration.js
@@ -1,6 +1,26 @@
-
-function add_character(){
-    let test = document.getElementById("class");
-    let value = test.value;
-    alert("O usuário escolheu a classe: " + value);
+class Character{
+    character_name;
+    character_class;
+    constructor(character_name, character_class){
+        this.character_name = character_name;
+        this.character_class = character_class;
+    }
 }
+
+class Manager{
+    constructor(){
+    }
+    add_character(){
+        let character_name = document.getElementById("name");
+        character_name = character_name.value;
+
+        let character_class = document.getElementById("class");
+        character_class = character_class.value;
+
+        const character = new Character();
+        alert("O usuário escolheu o nome: " + character_name);
+        alert("O usuário escolheu a classe: " + character_class);
+    }
+}
+
+const manager = new Manager();
