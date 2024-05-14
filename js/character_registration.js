2bb525712e58fd0bc9208eb6d812b5fcb1246d46
Railson Martins <railsonmartins1970@gmail.com>
2024-05-14 12:20:49 -0300

    Add character in localstorage

--- js/character_registration.js
+++ js/character_registration.js
@@ -1,3 +1,5 @@
+let characters = [];
+
 class Character{
     character_name;
     character_class;
@@ -10,6 +12,16 @@ class Character{
 class Manager{
     constructor(){
     }
+
+    get_character(){
+        if (localStorage.getItem('characters') === null) {
+            localStorage.setItem('characters', JSON.stringify(characters));
+        } else {
+            characters = JSON.parse(localStorage.getItem('characters'));
+        }
+        return characters;
+    }
+
     add_character(){
         let character_name = document.getElementById("name");
         character_name = character_name.value;
@@ -17,9 +29,20 @@ class Manager{
         let character_class = document.getElementById("class");
         character_class = character_class.value;
 
-        const character = new Character();
-        alert("O usuário escolheu o nome: " + character_name);
-        alert("O usuário escolheu a classe: " + character_class);
+        let character = new Character(character_name, character_class);
+
+        const characters = this.get_character();
+
+        characters.push(character);
+
+        let stringArray = JSON.stringify(characters);
+
+        localStorage.setItem('characters', stringArray);
+    }
+
+    show_character(){
+        // alert("O nome: " + character_list[0].character_name + ". Tem a classe: " + character_list[0].character_class);
+        alert("Ola! ");
     }
 }
 
