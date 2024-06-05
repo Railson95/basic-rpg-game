d9a1706d241b9edb93b1fd6eec438eb5763bd0ec
Railson Martins <railsonmartins1970@gmail.com>
2024-06-05 10:44:52 -0300

    Add basic img orc

--- js/data.js
+++ js/data.js
@@ -2,6 +2,10 @@ const characterData = {
     hero: {
         name: "Paladin",
         avatar: "/img/paladin.png",
+    },
+    monster: {
+        name: "Orc",
+        avatar: "/img/orc.png"
     }
 }
 export default characterData
--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ import characterData from "./data.js"
 
 function render() {
     document.getElementById('hero').innerHTML = paladin.getCharacterHtml();
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
 
 const paladin = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
 render();
